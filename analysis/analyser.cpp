2f0ce2e70c3727771c37b51456fc19df03caf30c
Mikhail <msladkov2002@gmail.com>
2021-11-20 13:46:54 +0300

    it works!

--- analysis/analyser.cpp
+++ analysis/analyser.cpp
@@ -4,11 +4,11 @@
 #include <cmath>
 #include <iostream>
 
-static size_t create_array_from_set(Variable** array, std::set<Variable*>* set) {
-    array = new Variable*[set -> size()];
-    Variable** ptr = array;
+static size_t create_array_from_set(Variable*** array, std::set<Variable*>* set) {
+    *array = new Variable*[set -> size()];
+    Variable** ptr = *array;
     for (Variable* v : *set) {
-        *array = v;
+        **array = v;
         array ++;
     }
     return set -> size();
@@ -17,25 +17,26 @@ static size_t create_array_from_set(Variable** array, std::set<Variable*>* set) 
 static void next_combination(Variable** array, size_t size) {
     bool value = array[0] -> get_value();
     array[0] -> set_value(!value);
-    std::cout << array[0];
+    std::cout << array[0] -> get_value();
     bool carry = value; 
     for (size_t i = 1; i < size; i++) {
         value = array[i] -> get_value();
         array[i] -> set_value(value^carry);
         carry = value && carry;
-        std::cout << array[i];
+        std::cout << array[i] -> get_value();
     }
 }
 
 enum options analyse(Expression* tree, std::set<Variable*>* variables_set) {
     Variable** variables_array = NULL;
-    size_t array_size = create_array_from_set(variables_array, variables_set);
+    size_t array_size = create_array_from_set(&variables_array, variables_set);
     bool satisfied = tree -> evaluate();
     bool unsatisfied = !(tree -> evaluate());
     bool value;
     for (size_t c = 0; c < std::pow(2, array_size) - 1; c++)
     {
         next_combination(variables_array, array_size);
+        std::cout << "incremented" << std::endl;
         value = tree -> evaluate();
         satisfied &= value;
         unsatisfied &= !value;
